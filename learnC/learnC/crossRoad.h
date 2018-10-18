ba2a650ce913209fe1dcb2319f20f872aa9850e9
jeffWoo <1756111854@qq.com>
2018-10-18 21:31:10 +0800

    first commit

---
+++ learnC/learnC/crossRoad.h
@@ -0,0 +1,13 @@
+//
+//  crossRoad.h
+//  learnC
+//
+//  Created by Zhifeng Wu on 2018/10/18.
+//  Copyright © 2018年 Zhifeng Wu. All rights reserved.
+//
+
+#ifndef crossRoad_h
+#define crossRoad_h
+
+
+#endif /* crossRoad_h */
---
+++ learnC/learnC/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  learnC
+//
+//  Created by Zhifeng Wu on 2018/10/18.
+//  Copyright © 2018年 Zhifeng Wu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    printf("jeffWoo say:Hello, World!\n");
+    return 0;
+}
